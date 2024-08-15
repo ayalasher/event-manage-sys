6e191f62791c71b520e1ed18da5d4a0813d66036
ayalasher <144620362+ayalasher@users.noreply.github.com>
2024-08-15 22:17:38 +0300

    styling the lading page

--- frontend/ems-front/src/components/Home.jsx
+++ frontend/ems-front/src/components/Home.jsx
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 export default function Home() {
     return <div>
         <h3>At home page</h3>
-        <Link to={'/'} >Home</Link>
+        {/* <Link to={'/'} >Home</Link> */}
         <Link to={'/login'} >Login</Link>
         <Link to={'/Signup'} >Signup </Link>
-        <Link to={'Asll'} >Asll</Link>
+        {/* Will come back to this and see how to fix it */}
+        {/* <Link to={'Asll'} >Asll</Link> */}
     </div>
 }
