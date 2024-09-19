17fe10de8cbdf297c0df467c420d350b3c64a676
ayalasher <144620362+ayalasher@users.noreply.github.com>
2024-09-19 19:46:23 +0300

    small changes

--- backend/hashing/Helper.mjs
+++ backend/hashing/Helper.mjs
@@ -3,11 +3,15 @@ import bcrypt from 'bcrypt'
 
 const saltrounds = 10
 
+
+// hashing funtion
 const hashpassword = (password)=>{
   const salt =   bcrypt.genSaltSync(saltrounds)
   return bcrypt.hashSync(password,salt)
 }
  
+
+// Password comparison funtion
 export const comparepassword = (plain,hashed)=>{
     return bcrypt.compareSync(plain,hashed) ;
 }
